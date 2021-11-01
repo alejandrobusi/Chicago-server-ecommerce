6791d2df6711cedb7f465d95cb9f7cd2e93329af
AlejandroBusi <alejandro_busi@hotmail.com>
2021-11-01 20:56:28 -0300

    Add new: validations and http request

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const { body } = require('express-validator');
-const { createProduct, deleteProduct } = require('../controllers/products');
+const { createProduct, deleteProduct, editProduct, getProducts } = require('../controllers/products');
 const { validationIsbn, validationMinMaxIsbn } = require('../helpers/validations');
 const route = Router()
 
@@ -19,9 +19,25 @@ body('imgUrl').trim().notEmpty().withMessage("url empty").isURL().withMessage('i
 
 createProduct)
 
+route.get('/products', getProducts)
+
 route.delete('/products', 
 body('isbn').isNumeric().withMessage('it is not number').notEmpty().withMessage("isnb empty"),
 
 deleteProduct )
+
+route.patch('/products/:prodId',
+body('name').trim().escape().notEmpty().withMessage("name empty").isLength({max : 60}).withMessage('maximum character is 60'),
+body('description').trim().escape().notEmpty().exists().withMessage("description empty").isLength({max : 2100}).withMessage('maximum character is 2100'),
+body('category').trim().escape().notEmpty().withMessage("category empty"),
+body('price').trim().escape().isNumeric().withMessage('it is not number').notEmpty().withMessage("price empty"), 
+body('author').trim().escape().notEmpty().withMessage("author empty").isLength({max : 40}).withMessage('maximum character is 30'),
+body('stock').trim().escape().isNumeric().withMessage('it is not number').notEmpty().withMessage("stock empty"), 
+body('isbn').isNumeric().withMessage('it is not number').notEmpty().withMessage("isnb empty"),
+body('isbn').custom(validationIsbn),
+body('editorial').trim().escape().notEmpty().withMessage("editorial empty"),
+body('fav').isBoolean().withMessage('it is not boolear').notEmpty().withMessage("fav empty"), 
+body('imgUrl').trim().notEmpty().withMessage("url empty").isURL().withMessage('it is not a url'),
+editProduct)
 
 module.exports = route 
