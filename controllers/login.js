38f2ef1cf1a440635a92583f2123e367c542df34
AlejandroBusi <alejandro_busi@hotmail.com>
2021-11-16 19:27:04 -0300

    fix controller

--- controllers/login.js
+++ controllers/login.js
@@ -11,29 +11,35 @@ const loginUser = async(req,res) =>{
   
   const searchEmail = await User.find({email: email})
   
-  const match = bcrypt.compareSync(password , searchEmail[0].password)
+  if(searchEmail.length > 0){
+
+    const match = bcrypt.compareSync(password , searchEmail[0].password)
+    
+    if (match) {
+      const payload = {
+        check:true
   
-  if (match) {
-    const payload = {
-      check:true
-
-    }
+      }
+    
+      const token = jwt.sign(payload, process.env.SECRET, {
+        expiresIn: '1h'
+      })
   
-    const token = jwt.sign(payload, process.env.SECRET, {
-      expiresIn: '1h'
-    })
-
-    const frontUser = {
-      name : searchEmail[0].name,
-      email: searchEmail[0].email,
-      admin : searchEmail[0].admin
+      const frontUser = {
+        name : searchEmail[0].name,
+        email: searchEmail[0].email,
+        admin : searchEmail[0].admin
+      }
+  
+      res.status(200).json({'msg':'Usuario logueado correctamente', frontUser, token: token})
+      return
+    } else {
+  
+      res.status(401).json({'msg':'Usuario o constraseña invalida perro'})
+  
     }
-
-    res.status(200).json({'msg':'Usuario logueado correctamente', frontUser, token: token})
-    return
   } else {
-
-    res.status(401).json({'msg':'Usuario o constraseña invalida perro'})
+    res.status(404).json({'msg':'Usuario inexistente'})
 
   }
 }
--- routes/purchases.js
+++ routes/purchases.js
@@ -9,18 +9,5 @@ route.post('/purchases', createPurchase)
 
 route.get('/purchases', getPurchases)
 
-route.patch('/purchases/:prodId', jwtvalidator,
-body('name').trim().escape().notEmpty().withMessage("name empty").isLength({max : 60}).withMessage('maximum character is 60'),
-body('description').trim().escape().notEmpty().exists().withMessage("description empty").isLength({max : 2100}).withMessage('maximum character is 2100'),
-body('category').trim().escape().notEmpty().withMessage("category empty"),
-body('price').trim().escape().isNumeric().withMessage('it is not number').notEmpty().withMessage("price empty"), 
-body('author').trim().escape().notEmpty().withMessage("author empty").isLength({max : 40}).withMessage('maximum character is 30'),
-body('stock').trim().escape().isNumeric().withMessage('it is not number').notEmpty().withMessage("stock empty"), 
-body('isbn').isNumeric().withMessage('it is not number').notEmpty().withMessage("isnb empty"),
-body('isbn').custom(validationIsbn),
-body('editorial').trim().escape().notEmpty().withMessage("editorial empty"),
-body('fav').isBoolean().withMessage('it is not boolear').notEmpty().withMessage("fav empty"), 
-body('imgUrl').trim().notEmpty().withMessage("url empty").isURL().withMessage('it is not a url'),
-editPurchases)
 
 module.exports = route 
